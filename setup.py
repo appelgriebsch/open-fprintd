d99c4a74e627cc74b045fbe4e82c60924b499883
Andreas Gerlach <andi@appelgriebsch.org>
2023-11-25 19:17:39 +0100

    update fedora spec

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ setup(name='open-fprintd',
       packages=['openfprintd'],
       scripts=[],
       data_files=[
-          ('share/dbus-1/system-services/', ['dbus_service/net.reactivated.Fprint.service']),
-          ('share/dbus-1/system.d/', ['dbus_service/net.reactivated.Fprint.conf']),
           ('lib/open-fprintd/', ['dbus_service/open-fprintd', 'scripts/suspend.py', 'scripts/resume.py']),
       ]
 )
